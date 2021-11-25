15e199b33a06512770449344ced9aaf23549b371
ViktoriiaV <vakhrinavo@gmail.com>
2021-11-25 20:50:07 +0200

    upd

--- tests.py
+++ tests.py
@@ -7,12 +7,16 @@ def connection():
     curs = conn.cursor()
     return curs
 
-#def test_Tables(connection):
-#    res_FK = connection.execute('SELECT name from sqlite_master where type= "table"').fetchall()
-#    res_list = [t[0] for t in res_FK]
- #   assert res_list == ['sqlite_sequence','Player_Attributes','Player','Match','League','Country',
- #                     'Team','Team_Attributes']
+def test_Tables(connection):
+    res_FK = connection.execute('SELECT name from sqlite_master where type= "table"').fetchall()
+    res_list = [t[0] for t in res_FK]
+    assert res_list == ['sqlite_sequence','Player_Attributes','Player','Match','League','Country',
+                      'Team','Team_Attributes']
 
 def test_County_total_records (connection):
     res_Country_total = connection.execute('SELECT COUNT(1) FROM Country').fetchone()
     assert res_Country_total[0] == 11
+
+def test_League_total_records (connection):
+    res_League_total = connection.execute('SELECT COUNT(1) FROM League').fetchone()
+    assert res_League_total[0] == 11
