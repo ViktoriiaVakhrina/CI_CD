d09b3b83c899a8962a5a8b189a6a7cb25cb80957
ViktoriiaV <vakhrinavo@gmail.com>
2021-11-25 14:30:05 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,6 +10,6 @@ def connection():
 def test_Tables(connection):
     res_FK = connection.execute('SELECT name from sqlite_master where type= "table"').fetchall()
     res_list = [t[0] for t in res_FK]
-    assert res_list == ['Player_Attributes','Player','Match','League','Country',
+    assert res_list == ['sqlite_sequence','Player_Attributes','Player','Match','League','Country',
                       'Team','Team_Attributes']
 
