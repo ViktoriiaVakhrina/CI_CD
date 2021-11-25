cf0a53793ef25a48467dcdfb2b666e012385e6c1
ViktoriiaV <vakhrinavo@gmail.com>
2021-11-25 15:01:31 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,9 +10,9 @@ def connection():
 def test_Tables(connection):
     res_FK = connection.execute('SELECT name from sqlite_master where type= "table"').fetchall()
     res_list = [t[0] for t in res_FK]
-    assert res_list == ['Player_Attributes','Player','Match','League','Country',
+    assert res_list == ['sqlite_sequence','Player_Attributes','Player','Match','League','Country',
                       'Team','Team_Attributes']
 
 def test_County_total_records (connection):
     res_Country_total = connection.execute('SELECT COUNT(1) FROM Country').fentone()
-    assert res_Country_total == 11
+    assert res_Country_total == 10
